0cdcae7b4a18db451e89ad284fb39a9958669d28
NikoSAN02 <dineshbohra@gmail.com>
2025-02-17 04:05:11 +0530

    added get

--- index.js
+++ index.js
@@ -11,6 +11,11 @@ app.post("/webhook", (req, res) => {
   res.status(200).send("Webhook received");
 });
 
+// Health check endpoint
+app.get('/', (req, res) => {
+    res.send('Webhook server is running');
+});
+
 app.listen(3000, () => {
   console.log("Webhook server running on port 3000");
 });
